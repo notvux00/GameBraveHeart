7175a18b6cb9ff1d0b17181d1ae5149c99518944
Lê Duy Vũ <djanh123456@gmail.com>
2024-05-17 09:33:40 +0700

    update

--- Brave Heart/character.h
+++ Brave Heart/character.h
@@ -36,24 +36,24 @@ SDL_Rect collisionBoxes[NUM_COLLISION_BOXES] = {
 bool checkCollision(float characterX, float characterY, int characterSize, float bulletX, float bulletY, int bulletSize) {
     float dx = characterX - bulletX;
     float dy = characterY - bulletY;
-    float distance = std::sqrt(dx * dx + dy * dy);
+    float distance = sqrt(dx * dx + dy * dy);
     return distance < (characterSize / 2 + bulletSize / 2);
 }
 
 bool checkCollisionBox(float characterX, float characterY, int characterSize, SDL_Rect box) {
 
-    float characterCenterX = characterX + characterSize / 2.0f;
-    float characterCenterY = characterY + characterSize / 2.0f;
+    float characterCenterX = characterX + characterSize / 2;
+    float characterCenterY = characterY + characterSize / 2;
 
 
-    float boxCenterX = box.x + box.w / 2.0f;
-    float boxCenterY = box.y + box.h / 2.0f;
+    float boxCenterX = box.x + box.w / 2;
+    float boxCenterY = box.y + box.h / 2;
 
 
     float dx = characterCenterX - boxCenterX;
     float dy = characterCenterY - boxCenterY;
-    float combinedHalfWidths = (characterSize / 2.0f) + (box.w / 2.0f);
-    float combinedHalfHeights = (characterSize / 2.0f) + (box.h / 2.0f);
+    float combinedHalfWidths = (characterSize / 2) + (box.w / 2);
+    float combinedHalfHeights = (characterSize / 2) + (box.h / 2);
 
 
     if (abs(dx) < combinedHalfWidths && abs(dy) < combinedHalfHeights) {
